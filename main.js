f98489e847e29b13332c7a3d819085b1ba6d5afa
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2024-05-02 17:29:55 +1200

    Update navigation mode

--- main.js
+++ main.js
@@ -1,45 +1,49 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import ratGlb from './assets/rat.glb'
+import human1Glb from './assets/human-wholebody.glb'
+import human2Glb from './assets/human-halfbody.glb'
 
 const routes = [
   {
     name: 'Quick start',
-    url: './quick-start.html'
+    url: 'https://modelviewer.dev/shared-assets/models/NeilArmstrong.glb'
   },
   {
     name: 'Rat',
-    url: './rat.html'
+    url: ratGlb
   },
   {
     name: 'Human whole',
-    url: './human-1.html'
+    url: human1Glb
   },
   {
     name: 'Human half',
-    url: './human-2.html'
+    url: human2Glb
   },
-  {
-    name: 'Demo',
-    url: './demo.html'
-  },
-  {
-    name: 'Background',
-    url: './background.html'
-  }
 ]
 
 let NavList = ''
 routes.forEach((route) => {
   NavList += `
     <li>
-      <a href="${route.url}">
+      <button data-url="${route.url}">
         ${route.name}
-      </a>
+      </button>
     </li>
   `
 })
+
+window.onload = () => {
+  const navBtns = document.querySelectorAll('nav li button')
+  navBtns.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+      const url = e.target.dataset.url
+
+      const modelViewerEl = document.querySelector('model-viewer')
+      if (modelViewerEl) modelViewerEl.src = url
+    })
+  })
+}
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -47,7 +51,18 @@ document.querySelector('#app').innerHTML = `
     <nav>
       ${NavList}
     </nav>
+    <div class="model-viewer-container">
+      <!-- Use it like any other HTML element -->
+      <model-viewer
+        alt="Neil Armstrong's Spacesuit from the Smithsonian Digitization Programs Office and National Air and Space Museum"
+        src="${routes[0].url}"
+        ar
+        environment-image="https://modelviewer.dev/shared-assets/environments/moon_1k.hdr"
+        poster="https://modelviewer.dev/shared-assets/models/NeilArmstrong.webp"
+        shadow-intensity="1"
+        camera-controls touch-action="pan-y"
+      >
+      </model-viewer>
+    </div>
   </div>
 `
-
-setupCounter(document.querySelector('#counter'))
